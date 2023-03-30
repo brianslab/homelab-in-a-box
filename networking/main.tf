3ca4bf4ec672bb133f61d663dbc53dfe02838cb8
brianslab <bdkuhn602@gmail.com>
2023-03-30 03:18:01 -0700

    create public subnets

--- networking/main.tf
+++ networking/main.tf
@@ -14,3 +14,15 @@ resource "aws_vpc" "hiab_vpc" {
     Name = "hiab_vpc-${random_integer.random.id}"
   }
 }
+
+resource "aws_subnet" "hiab_public_subnet" {
+  count                   = length(var.public_cidrs)
+  vpc_id                  = aws_vpc.hiab_vpc.id
+  cidr_block              = var.public_cidrs[count.index]
+  map_public_ip_on_launch = true
+  availability_zone       = ["us-west-2a", "us-west-2b", "us-west-2c", "us-west-2d"][count.index]
+
+  tags = {
+    Name = "hiab_public_${count.index + 1}"
+  }
+}

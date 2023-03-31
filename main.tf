2112fb6f759a354da245e10ad8e1f0d3f62b9264
brianslab <bdkuhn602@gmail.com>
2023-03-31 02:29:48 -0700

    move db name and db username to tfvars

--- main.tf
+++ main.tf
@@ -18,8 +18,8 @@ module "database" {
   db_storage             = 10
   db_engine_version      = "5.7.22"
   db_instance_class      = "db.t2.micro"
-  db_name                = "hiab-k3s-db"
-  db_user                = "brianslab"
+  db_name                = var.db_name
+  db_user                = var.db_user
   db_password            = var.db_password
   db_subnet_group_name   = module.networking.db_subnet_group_name[0]
   vpc_security_group_ids = module.networking.db_security_group
--- variables.tf
+++ variables.tf
@@ -4,10 +4,22 @@ variable "aws_region" {
   default = "us-west-2"
 }
 
+# Networking variables
 variable "access_ip" {
   type = string
 }
 
-variable "db_password" {
+# DB variables
+variable "db_name" {
   type = string
+}
+
+variable "db_user" {
+  type      = string
+  sensitive = true
+}
+
+variable "db_password" {
+  type      = string
+  sensitive = true
 }

2f37e70554a38c7caf977e387178b1b126199625
brianslab <bdkuhn602@gmail.com>
2023-03-31 03:31:33 -0700

    create loadbalancer target group

--- loadbalancing/main.tf
+++ loadbalancing/main.tf
@@ -6,3 +6,16 @@ resource "aws_lb" "hiab_loadbalancer" {
   security_groups = [var.public_security_group]
   idle_timeout    = 400
 }
+
+resource "aws_lb_target_group" "hiab_target_group" {
+  name     = "hiab-lb-target-group-${substr(uuid(), 0, 3)}"
+  port     = var.target_group_port
+  protocol = var.target_group_protocol
+  vpc_id   = var.vpc_id
+  health_check {
+    healthy_threshold   = var.lb_healthy_threshold
+    unhealthy_threshold = var.lb_unhealthy_threshold
+    timeout             = var.lb_timeout
+    interval            = var.lb_interval
+  }
+}
--- loadbalancing/variables.tf
+++ loadbalancing/variables.tf
@@ -2,3 +2,10 @@
 
 variable "public_subnets" {}
 variable "public_security_group" {}
+variable "target_group_port" {}
+variable "target_group_protocol" {}
+variable "vpc_id" {}
+variable "lb_healthy_threshold" {}
+variable "lb_unhealthy_threshold" {}
+variable "lb_timeout" {}
+variable "lb_interval" {}
